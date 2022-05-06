6d985792d324b33755311e11240293359429f161
lebania <lebania@doubled.kr>
2022-05-06 18:27:43 +0900

    Post 중간 저장.

--- src/main/java/com/example/demo/controller/GetAPIController.java
+++ src/main/java/com/example/demo/controller/GetAPIController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,15 @@ public class GetAPIController {
             return "유저의 아이디 또는 패스워드가 필요합니다.!";
         }
         return userId + "유저 아이디는 이거고" + password + "패스워드는 이거임";
+    }
+
+    @PostMapping("api")
+    public String postRequest(@RequestBody Map<String, String> myBody) {
+
+        System.out.println(myBody.get("name"));
+        System.out.println(myBody.get("password"));
+
+        return "이거 성공합니다";
     }
 
 }
